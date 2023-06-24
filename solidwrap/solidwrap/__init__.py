d5aaad839fd17002f1e412055622bc0075fad7df
Sean Yeatts <39303151+SeanYeatts@users.noreply.github.com>
2023-06-24 12:37:27 -0400

    Add files via upload

---
+++ solidwrap/solidwrap/__init__.py
@@ -0,0 +1,15 @@
+"""
+SolidWrap
+=========
+
+Copyright (c) 2023 Sean Yeatts. All rights reserved.
+
+This module provides an API for streamlined interfacing with SolidWorks
+and Enterprise PDM processes.
+"""
+
+__title__       = "SolidWrap"
+__author__      = "Sean Yeatts"
+__copyright__   = "Copyright (c) 2023 Sean Yeatts. All rights reserved."
+
+from .solidwrap import *
