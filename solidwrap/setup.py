c28ec2a4866164b49e635f893a1be68bb1d8998c
Sean Yeatts <39303151+SeanYeatts@users.noreply.github.com>
2023-06-24 12:57:12 -0400

    Update setup.py

--- solidwrap/setup.py
+++ solidwrap/setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='solidwrap',
-    version='0.0.1',
+    version='0.0.4',
     packages=find_packages(),
     install_requires=[
         'quickpathstr',
