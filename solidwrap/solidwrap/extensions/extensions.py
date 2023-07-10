97e6796f2f96e3c41e0535c8e3a9b8984a865d1e
Sean Yeatts <39303151+SeanYeatts@users.noreply.github.com>
2023-07-10 15:35:16 -0400

    Add files via upload

---
+++ solidwrap/solidwrap/extensions/extensions.py
@@ -0,0 +1,12 @@
+"""
+Copyright (c) 2023 Sean Yeatts. All rights reserved.
+"""
+
+# Import definition
+__all__ = [
+    "Extension"
+]
+
+# Base class for building extensions
+class Extension:
+    pass
